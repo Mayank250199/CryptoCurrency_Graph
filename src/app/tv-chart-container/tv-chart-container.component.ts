cfd7330c785155b895ede324776b22c23f509fb7
Mayank Sharma <mayanksharma250199@gmail.com>
2018-07-31 12:46:38 +0530

    minor change

--- src/app/tv-chart-container/tv-chart-container.component.ts
+++ src/app/tv-chart-container/tv-chart-container.component.ts
@@ -15,7 +15,7 @@ export class TvChartContainerComponent implements OnInit {
     private _symbol: ChartingLibraryWidgetOptions['symbol'] = 'AAPL';
     private _interval: ChartingLibraryWidgetOptions['interval'] = 'D';
     // BEWARE: no trailing slash is expected in feed URL
-    private _datafeedUrl = 'http://api.imdex.market/';
+    private _datafeedUrl = 'https://demo_feed.tradingview.com';
     private _libraryPath: ChartingLibraryWidgetOptions['library_path'] = '/assets/charting_library/';
     private _chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url'] = 'https://saveload.tradingview.com';
     private _chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version'] = '1.1';
